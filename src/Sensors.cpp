60cad9da9a0e2d41a2a0b44cc7f7c9015599ede0
Hanse-14 <johannes14.graf@gmail.com>
2023-11-02 21:00:49 +0100

    Fixed function readSoilMoisture

--- src/Sensors.cpp
+++ src/Sensors.cpp
@@ -21,23 +21,24 @@ float readBmeHumidity()
 // Funktion Temperatur außerhalb Gewächshaus
 float readNtcTemperature()
 {
-    float analogValue = (analogRead(PIN_SENSOR_NTC) / REFERENCE_VOLTAGE) * 1023;           // Einlesen der Spannung vom Spannungsteiler
-    float rNtc = (RESISTOR_R3 / REFERENCE_VOLTAGE) * analogValue;                   // Widerstand vom NTC
-    return (1 / ((log(rNtc/NTC_RN) / NTC_B) + 1 / (NTC_TN + 273.15)) - 273.15);     // Umrechnung von Rt = Rn * e hoch B*(1/T - 1/Tn)    
+    float analogValue = (analogRead(PIN_SENSOR_NTC) / REFERENCE_VOLTAGE) * 1023;            // Einlesen der Spannung vom Spannungsteiler
+    float rNtc = (RESISTOR_R3 / REFERENCE_VOLTAGE) * analogValue;                           // Widerstand vom NTC
+    return (1 / ((log(rNtc/NTC_RN) / NTC_B) + 1 / (NTC_TN + 273.15)) - 273.15);             // Umrechnung von Rt = Rn * e hoch B*(1/T - 1/Tn)    
 }
 
 // Funktion Bodenfeuchte Gewächshaus 0 - 100%
 float readSoilMoisture()
 {
     float soilHumidity = analogRead(PIN_SENSOR_SOIL_HUMIDITY);         
-    return (soilHumidity, 0, 1023, 0, 100);                  //Feuchtigkeitswerte von 0% - 100%
+    return map(soilHumidity, 0, 1023, 0, 100);                  //Feuchtigkeitswerte von 0% - 100%
 }
 
 // Funktion Batteriespannung messen
 float readBatteryVoltage()
 {
-    float auxiliaryVariable = 0;    // Hilfsvariable zum Einlesen der Spannung
+    float auxiliaryVariable = 0;   
 
-    auxiliaryVariable = (analogRead(PIN_SENSOR_BATTERY_VOLTAGE) * REFERENCE_VOLTAGE) / 1024;       //Einlesen der Spannung und gemäß Spannungsteiler umrechnen
+    //Einlesen der Spannung und gemäß Spannungsteiler umrechnen
+    auxiliaryVariable = (analogRead(PIN_SENSOR_BATTERY_VOLTAGE) * REFERENCE_VOLTAGE) / 1024;       
     return (auxiliaryVariable * (RESISTOR_R1 + RESISTOR_R2)) / RESISTOR_R2;           
 }
