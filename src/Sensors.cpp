b0e020515f77e4fb67f4a0888c0b17b8fd405752
Hanse-14 <johannes14.graf@gmail.com>
2023-11-02 20:05:22 +0100

    Added function NtcTemperatur

--- src/Sensors.cpp
+++ src/Sensors.cpp
@@ -6,9 +6,23 @@
 
 BME280I2C bme;
 
-
-//return Temperatur in °C
+// Funktion Temperatur
 float readBmeTemperature()
 {
-    return bme.temp();
+    return bme.temp();  // in °C
+}
+
+// Funktion Luftfeuchte Gewächshaus
+float readBmeHumidity()
+{
+    return bme.hum();
+}
+
+// Funktion Temperatur außerhalb Gewächshaus
+float readNtcTemperature()
+{
+    float analogValue = (analogRead(PIN_NTC) / REFERENCE_VOLTAGE) * 1023;           // Einlesen der Spannung vom Spannungsteiler
+    float rNtc = (RESISTOR_R3 / REFERENCE_VOLTAGE) * analogValue;                   // Widerstand vom NTC
+    return (1 / ((log(rNtc/NTC_RN) / NTC_B) + 1 / (NTC_TN + 273.15)) - 273.15);     // Umrechnung von Rt = Rn * e hoch B*(1/T - 1/Tn)    
+   
 }
