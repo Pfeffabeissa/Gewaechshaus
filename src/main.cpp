686e38a72216439b6a3e16ebfede7777254440b9
Hanse-14 <johannes14.graf@gmail.com>
2023-11-09 21:47:42 +0100

    Changed PIN_UI_BUTTON_BOTTOM and PIN_UI_BUTTON_TOP

--- src/main.cpp
+++ src/main.cpp
@@ -33,8 +33,8 @@ void setup() {
 
 void loop() {
     manageDisplayfunctions();
-    arrayMeasure();
-    actualMeasure();
-    manageMotors();
-    managePowerSupplies();
+    // arrayMeasure();
+    // actualMeasure();
+    // manageMotors();
+    // managePowerSupplies();
 }
